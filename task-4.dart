7a8ae28388f31b5c180148c001bc03c6a1323dd6
AlexGrigorenco <alpsnhandy@gmail.com>
2024-02-21 02:19:26 +0200

    task 4

--- task-4.dart
+++ task-4.dart
@@ -1,20 +1,41 @@
-//! Написать функцию, принимающую год (int), возвращающую bool. Результат функции должен отражать является ли год високосным.
+// Можно использовать шаблон проектирования "Фасад" (Facade):
 
-void main() {
-  print(isYeapYear(2024));
-  print(isYeapYear(2023));
-  print(isYeapYear(2022));
-  print(isYeapYear(2021));
-  print(isYeapYear(2020));
+// Упрощение интерфейса: Фасад позволяет предоставить упрощенный интерфейс к сложной системе. В случае с авторизацией, фасад может скрыть сложную логику проверки подлинности, обработки сеансов и управления доступом.
+
+// Изоляция деталей реализации: Фасад позволяет изолировать клиентский код от деталей реализации авторизации. Это делает код более чистым и уменьшает зависимость клиента от конкретной реализации авторизации.
+
+// Управление различными способами авторизации: Фасад может обеспечить единый интерфейс для работы с различными способами авторизации, такими как логин/пароль, OAuth и другими.
+
+// Фасад для работы с авторизацией
+class AuthFacade {
+  bool isAuthenticated = false;
+
+  // Метод для аутентификации пользователя
+  void authenticate(String username, String password) {
+    // Реализация аутентификации
+    isAuthenticated = (username == 'admin' && password == 'password');
+  }
+
+  // Метод для проверки авторизации пользователя
+  bool checkAuthentication() {
+    return isAuthenticated;
+  }
+
+  // Метод для выхода пользователя из системы
+  void signOut() {
+    isAuthenticated = false;
+  }
 }
 
-bool isYeapYear(int year) {
-  if (year % 4 != 0) {
-    return false;
-  } else if (year % 100 != 0) {
-    return true;
-  } else if (year % 400 != 0) {
-    return false;
-  }
-  return true;
+void main() {
+  AuthFacade authFacade = AuthFacade();
+
+  authFacade.authenticate('admin', 'password');
+  print('Is authenticated: ${authFacade.checkAuthentication()}');
+
+  authFacade.signOut();
+  print('Is authenticated: ${authFacade.checkAuthentication()}');
+
+  authFacade.authenticate('qwerty', 'password');
+  print('Is authenticated: ${authFacade.checkAuthentication()}');
 }
